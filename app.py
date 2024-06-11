dcb52fac2f941d5ad032592030d22d6465ab623d
abhayw1311 <98542901+abhayw1311@users.noreply.github.com>
2024-06-11 23:18:03 +0530

    updated

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import MySQLdb.cursors
 from flask import Flask, render_template,request,session,url_for,redirect,flash, send_file
 from nltk.corpus import stopwords
 import nltk
+import pickle
 nltk.download('stopwords')
 
 app = Flask(__name__)
@@ -19,11 +20,58 @@ app.config['MYSQL_DB']='diseases_db'
 
 mysql = MySQL(app)
 
+#List of the symptoms is listed here in list l1.
+
+l1=['back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
+    'yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach',
+    'swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation',
+    'redness_of_eyes','sinus_pressure','runny_nose','congestion','chest_pain','weakness_in_limbs',
+    'fast_heart_rate','pain_during_bowel_movements','pain_in_anal_region','bloody_stool',
+    'irritation_in_anus','neck_pain','dizziness','cramps','bruising','obesity','swollen_legs',
+    'swollen_blood_vessels','puffy_face_and_eyes','enlarged_thyroid','brittle_nails',
+    'swollen_extremeties','excessive_hunger','extra_marital_contacts','drying_and_tingling_lips',
+    'slurred_speech','knee_pain','hip_joint_pain','muscle_weakness','stiff_neck','swelling_joints',
+    'movement_stiffness','spinning_movements','loss_of_balance','unsteadiness',
+    'weakness_of_one_body_side','loss_of_smell','bladder_discomfort','foul_smell_of urine',
+    'continuous_feel_of_urine','passage_of_gases','internal_itching','toxic_look_(typhos)',
+    'depression','irritability','muscle_pain','altered_sensorium','red_spots_over_body','belly_pain',
+    'abnormal_menstruation','dischromic _patches','watering_from_eyes','increased_appetite','polyuria','family_history','mucoid_sputum',
+    'rusty_sputum','lack_of_concentration','visual_disturbances','receiving_blood_transfusion',
+    'receiving_unsterile_injections','coma','stomach_bleeding','distention_of_abdomen',
+    'history_of_alcohol_consumption','fluid_overload','blood_in_sputum','prominent_veins_on_calf',
+    'palpitations','painful_walking','pus_filled_pimples','blackheads','scurring','skin_peeling',
+    'silver_like_dusting','small_dents_in_nails','inflammatory_nails','blister','red_sore_around_nose',
+    'yellow_crust_ooze']
+
+#List of Diseases is listed in list disease.
+
+disease=['Fungal infection', 'Allergy', 'GERD', 'Chronic cholestasis',
+       'Drug Reaction', 'Peptic ulcer diseae', 'AIDS', 'Diabetes ',
+       'Gastroenteritis', 'Bronchial Asthma', 'Hypertension ', 'Migraine',
+       'Cervical spondylosis', 'Paralysis (brain hemorrhage)', 'Jaundice',
+       'Malaria', 'Chicken pox', 'Dengue', 'Typhoid', 'hepatitis A',
+       'Hepatitis B', 'Hepatitis C', 'Hepatitis D', 'Hepatitis E',
+       'Alcoholic hepatitis', 'Tuberculosis', 'Common Cold', 'Pneumonia',
+       'Dimorphic hemmorhoids(piles)', 'Heart attack', 'Varicose veins',
+       'Hypothyroidism', 'Hyperthyroidism', 'Hypoglycemia',
+       'Osteoarthristis', 'Arthritis',
+       '(vertigo) Paroymsal  Positional Vertigo', 'Acne',
+       'Urinary tract infection', 'Psoriasis', 'Impetigo']
+
+#disease = [df['prognosis'].unique()]
+#print(disease)
+
+with open('models/decision_tree.pkl','rb') as f:
+    decision_tree=pickle.load(f)
+
 
 def detect_disease(symptoms):
     # Here you would implement the logic to detect diseases based on symptoms
     # For demonstration purposes, let's return a dummy list of diseases
     # This is a simplistic mapping and would typically be more complex in a real-world scenario
+     #sample array of symptoms, take this as a input from the user but ensure that symptoms is present in the given dataset
+    print([decision_tree.predict(symptoms)[0]])
+    return [decision_tree.predict(symptoms)[0]]
     disease_mapping = {
         "cold": ["Flu"],
         "cough": ["Flu"],
@@ -259,6 +307,18 @@ def get_specialist(disease):
 @app.route('/')
 def index():
     # flash("Welcome to web application")
+    l2=[]
+    for i in range(0,len(l1)):
+        l2.append(0)
+    psymptoms = ['obesity', 'swollen_legs','malaise',]
+    for k in range(0,len(l1)):
+        for z in psymptoms:
+            if(z==l1[k]):
+                l2[k]=1
+
+    #disease prediction
+    inputtest = [l2]
+    print(detect_disease(inputtest))
     return render_template('index.html')
 
 
@@ -292,7 +352,7 @@ def audio():
                 if word  not in stopwords.words('english'):
                     symptoms.append(word)
             flash("Symptoms recognized: " + ', '.join(symptoms))
-            
+
 
             # Detect disease based on symptoms
             CD = detect_disease(symptoms)
